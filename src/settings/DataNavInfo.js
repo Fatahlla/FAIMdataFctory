39a52afecd0b076986ee82318b75a29082cefb47
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-02-29 21:43:41 +0600

    🏷️📕added data nav array

---
+++ src/settings/DataNavInfo.js
@@ -0,0 +1,18 @@
+export const DataNavInfo = [
+  {
+    title: "Data Card",
+    value: "dataCard",
+  },
+  {
+    title: "Code",
+    value: "code",
+  },
+  {
+    title: "Discussion",
+    value: "discussion",
+  },
+  {
+    title: "Suggestion",
+    value: "suggestion",
+  },
+];
